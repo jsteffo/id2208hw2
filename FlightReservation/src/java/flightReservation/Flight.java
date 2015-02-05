45304bf5631ffa753e13e22f3e32a3703415bd93
norden-oscar <norden.oscar@gmail.com>
2015-02-05 23:56:01 +0100

    Skapade projektet och började på Flight classen

---
+++ FlightReservation/src/java/flightReservation/Flight.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flightReservation;
+
+/**
+ *
+ * @author norde_000
+ */
+public class Flight {
+    String destinationCity;
+    String departureCity;
+    int flightLenght;
+    LocalTime departureTime;    // LocalTime finns bara i java 8, ska man köra det eller ngt annat?
+    int price;
+    
+    
+    
+}
