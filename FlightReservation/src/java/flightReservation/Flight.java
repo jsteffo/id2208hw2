b984b525f221fc449d898d00491909e36513237e
norden-oscar <norden.oscar@gmail.com>
2015-02-06 14:17:42 +0100

    Gjort klart Flight och Route, bytt namn på servicen

--- FlightReservation/src/java/flightReservation/Flight.java
+++ FlightReservation/src/java/flightReservation/Flight.java
@@ -6,26 +6,69 @@
 package flightReservation;
 
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 /**
  *
  * @author norde_000
  */
 public class Flight {
-    String destinationCity;
-    String departureCity;
-    int flightLenght;
-    LocalTime departureTime;    // LocalTime finns bara i java 8, ska man köra det eller ngt annat?
-    LocalTime estimatedArrival;
-    int price;
+    private String destinationCity;
+    private String departureCity;
+    private long flightLenght;      // Tid flyget tar i minuter
+    private LocalTime departureTime;    // LocalTime finns bara i java 8, ska man köra det eller ngt annat?
+    private LocalTime estimatedArrival;
+    private int price;
     
-    public Flight(String destinationCity,String departureCity,int flightLenght,LocalTime departureTime,int price){
+    public Flight(String destinationCity,String departureCity,long flightLenght,LocalTime departureTime,int price){
         this.destinationCity = destinationCity;
         this.departureCity = departureCity;
         this.flightLenght = flightLenght;
-        this.departureTime = departureTime;
+        this.departureTime = departureTime.truncatedTo(ChronoUnit.MINUTES);       // gör så vi bara får med HH:MM
         this.price = price;
-        this.estimatedArrival = departureTime.plus(flightLenght, null);
+        this.estimatedArrival = departureTime.plus(flightLenght,ChronoUnit.MINUTES);    // adderar flightlenght antal minuter till tiden
+    }
+
+    /**
+     * @return the destinationCity
+     */
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    /**
+     * @return the departureCity
+     */
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    /**
+     * @return the flightLenght
+     */
+    public long getFlightLenght() {
+        return flightLenght;
+    }
+
+    /**
+     * @return the departureTime
+     */
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    /**
+     * @return the estimatedArrival
+     */
+    public LocalTime getEstimatedArrival() {
+        return estimatedArrival;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
     }
     
     
--- FlightReservation/src/java/flightReservation/Route.java
+++ FlightReservation/src/java/flightReservation/Route.java
@@ -5,6 +5,7 @@
  */
 package flightReservation;
 
+import java.time.Duration;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
@@ -13,10 +14,61 @@ import java.util.ArrayList;
  * @author norde_000
  */
 public class Route {
-    String departureCity;
-    String destinationCity;
-    LocalTime departureTime;
-    int routeTime;
-    ArrayList<Flight> flights; 
+    private String departureCity;
+    private String destinationCity;
+    private LocalTime departureTime;
+    private LocalTime destinationTime;
+    private Duration routeTime;
+    private ArrayList<Flight> flights; 
+    public Route(ArrayList<Flight> flights){
+        this.flights = flights;
+        this.departureCity = flights.get(0).getDepartureCity();
+        this.destinationCity = flights.get(flights.size()-1).getDestinationCity();
+        this.departureTime = flights.get(0).getDepartureTime();
+        this.destinationTime = flights.get(flights.size()-1).getEstimatedArrival();
+        this.routeTime = Duration.between(departureTime,destinationTime);
+    }
+
+    /**
+     * @return the departureCity
+     */
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    /**
+     * @return the destinationCity
+     */
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    /**
+     * @return the departureTime
+     */
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    /**
+     * @return the destinationTime
+     */
+    public LocalTime getDestinationTime() {
+        return destinationTime;
+    }
+
+    /**
+     * @return the routeTime
+     */
+    public Duration getRouteTime() {
+        return routeTime;
+    }
+
+    /**
+     * @return the flights
+     */
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
     
 }
